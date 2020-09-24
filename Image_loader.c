a048a1d4a62592cbdfad07727c55989bf107d235
direckton <direcktos@gmail.com>
2020-09-24 17:58:59 +0200

    first succesfull read

--- Image_loader.c
+++ Image_loader.c
@@ -4,3 +4,19 @@
 #include "Image_loader.h"
 
 
+int readFile()
+{
+	BMPHeaderInfo bmpFileInfo;
+
+	FILE* stream;
+
+	fopen_s(&stream, "test.bmp", "rb");
+	if (stream == NULL) {
+		printf("Error: the file could not be opened");
+		return 3;
+	}
+	fread(&bmpFileInfo, sizeof(BMPHeaderInfo), 1, stream);
+	
+
+	fclose(stream);
+}
--- Main.c
+++ Main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int readFile()
-{
-	int line[50] = { 0 };
-	FILE* stream;
+#include "Image_loader.h"
 
-
-	fopen_s(&stream, "test.bmp", "rb");
-	if (stream == 0)
-		return 3;
-	fread(line, sizeof(int), sizeof(line), stream);
-
-	fclose(stream);
-	for (int i = 0; i < 50; i++)
-	{
-		printf("0x%x\n", line[i]);
-		
-
-	}
-	printf("%p\n", &line[0]);
-}
+//int readFile()
+//{
+//	int line[50] = { 0 };
+//	FILE* stream;
+//
+//
+//	fopen_s(&stream, "test.bmp", "rb");
+//	if (stream == 0)
+//		return 3;
+//	fread(line, sizeof(int), sizeof(line), stream);
+//
+//	fclose(stream);
+//	for (int i = 0; i < 50; i++)
+//	{
+//		printf("0x%x\n", line[i]);
+//		
+//
+//	}
+//	printf("%p\n", &line[0]);
+//}
 
 
 int main()
